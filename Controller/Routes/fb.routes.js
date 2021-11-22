9b54e2de511ddaba5edc78799c398673213d4934
Ritwik Neema <ritwikneema651@gmail.com>
2021-11-22 15:16:37 +0530

    Facebook OAuth added

--- Controller/Routes/fb.routes.js
+++ Controller/Routes/fb.routes.js
@@ -1,3 +1,14 @@
 const Router = require('express').Router();
+const passport = require('passport');
+
+require('../Auth/fb.passport');
+
+Router.get('/', passport.authenticate('facebook', {
+    scope: ['public_profile', 'email']
+}));
+
+Router.get('/redirect', passport.authenticate('facebook') ,(req, res) => {
+    res.redirect('/dashboard');
+});
 
 module.exports = Router;
