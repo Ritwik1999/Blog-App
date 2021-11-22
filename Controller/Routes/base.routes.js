bb174eed9377c4b6003cabf602ed17b68b5e5a59
Ritwik Neema <ritwikneema651@gmail.com>
2021-11-22 19:07:57 +0530

    Dashboard done

--- Controller/Routes/base.routes.js
+++ Controller/Routes/base.routes.js
@@ -1,6 +1,7 @@
 // localhost:3000/<Route>
 const Router = require('express').Router();
 const Article = require('../../Model/article.model');
+const User = require('../../Model/user.model');
 
 Router.get('/', (req, res) => {
     if (req.session.user) {
@@ -23,13 +24,38 @@ Router.get('/new_article', (req, res) => {
 });
 
 Router.get('/search/:tag', (req, res) => {
-    Article.find({ tags: req.params.tag }).populate('author', 'username').then(articles => {
-        res.render('search', { articles, });
-    });
+    Article.find({ tags: req.params.tag })
+        .populate('author', 'username')
+        .then(articles => {
+            res.render('search', { articles, });
+        });
 });
 
 Router.get('/search', (req, res) => {
     res.render('search', { articles: [] });
+});
+
+Router.get('/dashboard', (req, res) => {
+    if (req.session.user || req.user) {
+        let user = req.session.user || req.user;
+        let usr_ids = [];
+        User.find({ username: { $in: user.people_you_are_following } })
+            .then(usrs => {
+                usrs.forEach(usr => usr_ids.push(usr._id));
+            })
+            .then(() => {
+                Article.find({ author: { $in: usr_ids } })
+                    .populate('author', ['username'])
+                    .then(articles => {
+                        res.render('dashboard', { articles, });
+                    });
+            })
+            .catch(err => {
+                res.redirect('/');
+            });
+    } else {
+        res.redirect('/');
+    }
 });
 
 module.exports = Router;
